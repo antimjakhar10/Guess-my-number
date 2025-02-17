4aa6d262e11b48a7a5441ab75d95a9ffe75b6688
antimjakhar10 <jakharantim1710@gmail.com>
2025-02-17 21:30:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// if user is equal to our num we will win otherwise we will loose
+
+let computerNumber = Number(prompt("Choose any number from 1 to 100"));
+let ourNumber = 4;
+if(computerNumber == ourNumber){
+    console.log("You Win!");
+}else{
+    console.log("You loose! choose again...");
+};
